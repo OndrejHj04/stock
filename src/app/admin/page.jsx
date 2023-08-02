3e77189b8386319c4dd72c6e397918887ae35a83
OndrejHj04 <ondra.hajku@seznam.cz>
2023-08-02 07:26:13 +0200

    save

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,12 +1,18 @@
 "use client";
-import { Button, Paper, Slider, Typography } from "@mui/material";
+import {
+  Button,
+  CircularProgress,
+  Paper,
+  Slider,
+  Typography,
+} from "@mui/material";
 import React, { useEffect } from "react";
 import { collection, doc, onSnapshot, updateDoc } from "firebase/firestore";
 import { db } from "../../../db";
 import { useState } from "react";
 
 export default function Admin() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
   const [slider, setSlider] = useState(1);
   useEffect(() => {
     onSnapshot(collection(db, "items"), (item) => {
@@ -30,40 +36,50 @@ export default function Admin() {
 
   return (
     <Paper className="p-3 max-w-md mx-auto flex flex-col gap-2 items-center">
-      <Typography variant="h4">
-        Předměty na burze
-      </Typography>
-      {data.map(({ name, count }, i) => (
-        <React.Fragment key={i}>
-          <div className="flex gap-3 items-center">
-            <div className="mr-auto">
-              <Typography variant="h6">Zbraň</Typography>
-            </div>
-            <div>
-              <Typography>POČET: {count}</Typography>
-            </div>
-            <div>
-              <Button
-                onClick={() => handleIncrement(name, count)}
-                variant="contained"
-              >
-                <Typography>Přidat</Typography>
-              </Button>
-            </div>
-            <div>
-              <Button
-                onClick={() => handleDecrement(name, count)}
-                variant="contained"
-              >
-                <Typography>Odebrat</Typography>
-              </Button>
-            </div>
-          </div>
-        </React.Fragment>
-      ))}
-      <Typography variant="h6">
-        Hodnota slideru: {slider}
-      </Typography>
+      <Typography variant="h4">Předměty na burze</Typography>
+      {Array.isArray(data) ? (
+        <>
+          {data.length ? (
+            <>
+              {data.map(({ name, count }, i) => (
+                <React.Fragment key={i}>
+                  <div className="flex gap-3 items-center">
+                    <div className="mr-auto">
+                      <Typography variant="h6">Zbraň</Typography>
+                    </div>
+                    <div>
+                      <Typography>POČET: {count}</Typography>
+                    </div>
+                    <div>
+                      <Button
+                        onClick={() => handleIncrement(name, count)}
+                        variant="contained"
+                      >
+                        <Typography>Přidat</Typography>
+                      </Button>
+                    </div>
+                    <div>
+                      <Button
+                        onClick={() => handleDecrement(name, count)}
+                        variant="contained"
+                      >
+                        <Typography>Odebrat</Typography>
+                      </Button>
+                    </div>
+                  </div>
+                </React.Fragment>
+              ))}
+            </>
+          ) : (
+            <>
+              <Typography>žádná data v databázi</Typography>
+            </>
+          )}
+        </>
+      ) : (
+        <CircularProgress />
+      )}
+      <Typography variant="h6">Hodnota slideru: {slider}</Typography>
       <Slider
         value={slider}
         valueLabelDisplay="auto"
