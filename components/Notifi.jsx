0681153ddc598e91c89a7a0136b40d1bf1ce6d49
OndrejHj04 <ondra.hajku@seznam.cz>
2023-08-08 23:52:24 +0200

    notifi

--- components/Notifi.jsx
+++ components/Notifi.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Button } from "@mui/material";
+import addNotification from 'react-push-notification';
 
 export default function Notifi() {
-    
-    return (
-        <div>
-            <Typography>asfd</Typography>
-        </div>
-    )
+  const handleClick = () => {
+    addNotification({
+      title: "Warning",
+      subtitle: "This is a subtitle",
+      message: "This is a very long message",
+      theme: "darkblue",
+      native: true, // when using native, your OS will handle theming.
+    });
+  };
+  return (
+    <div>
+      <Button variant="contained" color="primary" onClick={handleClick}>
+        notifi
+      </Button>
+    </div>
+  );
 }
