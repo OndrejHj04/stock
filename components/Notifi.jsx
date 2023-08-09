e9ff3a749ffab709e78adcf57c32a7c6a45c126f
OndrejHj04 <ondra.hajku@seznam.cz>
2023-08-09 14:29:36 +0200

    deploy

--- components/Notifi.jsx
+++ components/Notifi.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 import { Button } from "@mui/material";
-import addNotification from 'react-push-notification';
 
 export default function Notifi() {
   const handleClick = () => {
-    addNotification({
-      title: "Warning",
-      subtitle: "This is a subtitle",
-      message: "This is a very long message",
-      theme: "darkblue",
-      native: true, // when using native, your OS will handle theming.
-    });
+
   };
   return (
     <div>
