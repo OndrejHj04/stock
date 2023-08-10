0a3424e2f0acc92bb28d2526d415ab219b195e96
OndrejHj04 <ondra.hajku@seznam.cz>
2023-08-10 08:11:39 +0200

    save

--- components/Notifi.jsx
+++ components/Notifi.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { Button } from "@mui/material";
-
+import addNotification from "react-push-notification";
 export default function Notifi() {
   const handleClick = () => {
-
+    addNotification({
+      title: "Warning",
+      message: "This is a very long message",
+      duration: 4000,
+      icon: "/check.png",
+      native: true,
+    }).then((res) => console.log(res));
   };
   return (
     <div>
