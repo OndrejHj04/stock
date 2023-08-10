69c7a9c95392a069d2a1d9b56eb353d287f92bd0
OndrejHj04 <ondra.hajku@seznam.cz>
2023-08-10 08:21:40 +0200

    save

--- components/Notifi.jsx
+++ components/Notifi.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button } from "@mui/material";
-import addNotification from "react-push-notification";
+
 export default function Notifi() {
   const handleClick = () => {
-    addNotification({
-      title: "Warning",
-      message: "This is a very long message",
-      duration: 4000,
-      icon: "/check.png",
-      native: true,
-    }).then((res) => console.log(res));
+    new Notification("Hey");
   };
+
+  useEffect(() => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission();
+    }
+  }, []);
+
   return (
     <div>
       <Button variant="contained" color="primary" onClick={handleClick}>
