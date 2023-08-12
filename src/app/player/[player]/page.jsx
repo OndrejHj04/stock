8de3a51d62ffdb152261902c067497a7e3146002
Ondřej Hájek <ondra.hajku@seznam.cz>
2023-08-12 08:59:09 +0200

    save

--- src/app/player/[player]/page.jsx
+++ src/app/player/[player]/page.jsx
@@ -136,7 +136,7 @@ export default function Page({ params: { player } }) {
               return (
                 <Card className="flex gap-2 p-2" key={i}>
                   <div className="flex gap-2 items-center">
-                    <Avatar sx={{ width: "70px", height: "70px" }} />
+                    <Avatar sx={{ width: "70px", height: "70px" }} src={`./${item.code}.png`}/>
                     <div className="flex flex-col">
                       <div className="flex items-center gap-2">
                         <Typography variant="h6">{item.label}</Typography>
